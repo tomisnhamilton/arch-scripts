5f9b90e767a9eb1a112f1f3f4d4fb0cd183974c1
Tomis Hamilton <tomisnhamilton@gmail.com>
2024-09-16 07:58:28 -0600

    added "must have" to install

--- fish-installer.sh
+++ fish-installer.sh
@@ -8,4 +8,6 @@ sudo pacman -Syu git fish curl
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
+omf i bang-bang
+
 echo "Oh-my-fish has been installed and is ready for use"
