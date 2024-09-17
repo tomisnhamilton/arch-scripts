f86f50f4871b5703861156af394f16f4005e6fe9
Tomis Hamilton <tomisnhamilton@gmail.com>
2024-09-17 06:42:01 -0600

    Changed update order

--- nvidia-installer.sh
+++ nvidia-installer.sh
@@ -2,7 +2,10 @@
 
 echo "This script will install the NVIDIA drivers for your system"
 
+# Install linux-headers
+sudo pacman -Syu linux-headers
+
 # Install the NVIDIA drivers
-sudo pacman -Syu nvidia-dkms nvidia-utils nvidia-settings libglvnd opencl-nvidia lib32-nvidia-utils lib32-libglvnd lib32-opencl-nvidia
+sudo pacman -S nvidia-dkms nvidia-utils nvidia-settings libglvnd opencl-nvidia lib32-nvidia-utils lib32-libglvnd lib32-opencl-nvidia
 
 echo "DONE!"
