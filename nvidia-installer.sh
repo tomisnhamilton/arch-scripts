94b71f5eb2d5a41de05a39fd5008611f25e00f5a
Tomis Hamilton <tomisnhamilton@gmail.com>
2024-10-07 21:46:26 -0600

    script tweak

--- nvidia-installer.sh
+++ nvidia-installer.sh
@@ -2,8 +2,12 @@
 
 echo "This script will install the NVIDIA drivers for your system"
 
+# Update the system
+sudo pacman -Syyu
+
+
 # Install linux-headers
-sudo pacman -Syu linux-headers
+sudo pacman -S linux-headers
 
 # Install the NVIDIA drivers
 sudo pacman -S nvidia-dkms nvidia-utils nvidia-settings libglvnd opencl-nvidia lib32-nvidia-utils lib32-libglvnd lib32-opencl-nvidia
